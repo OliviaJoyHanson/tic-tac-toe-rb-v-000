1b4246d83576f0354a98665fd41a9ee490042d1d
Olivia Hanson <olivia.joy.hanson@gmail.com>
2020-04-16 13:02:51 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -43,6 +43,17 @@ def move(board, index, token)
   end
 end
 
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if move(board, index, token = "X")
+    display_board(board)
+  else
+    turn(board)
+  end
+end
+
 def turn_count(board)
   counter = 0
   board.each do |i|
